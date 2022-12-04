fccd25a6c8e3217063c084f88e60ed6c43faa3ec
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-12-03 23:29:58 -0500

    Add fake data

---
+++ onboardingData.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: '1',
+    title: 'odio consequat varius integer ac',
+    description:
+      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+  },
+  {
+    id: '2',
+    title: 'leo rhoncus sed vestibulum	',
+    description: 'Etiam vel augue.',
+  },
+  {
+    id: '3',
+    title: 'nullam orci pede venenatis non',
+    description: 'Quisque id justo sit amet sapien dignissim vestibulum.',
+  },
+  {
+    id: '4',
+    title: 'ipsum ac',
+    description:
+      'In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.',
+  },
+  {
+    id: '5',
+    title: 'volutpat eleifend donec',
+    description:
+      '	In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl.',
+  },
+];
